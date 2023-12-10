035336dd11be2312baefa538892266769d527970
AdtSri413 <aditi.srinivasan@gmail.com>
2023-12-09 22:36:04 -0800

    Created lung_cancer.py. Added lung cancer dataset. Added test set split to breast_cancer.py

--- lung_cancer.py
+++ lung_cancer.py
@@ -7,12 +7,12 @@ from sklearn.model_selection import train_test_split
 RANDOM_STATE = 3
 
 # Import dataset
-data = pd.read_csv("breast-cancer.csv")
-y = data['diagnosis'].replace({'B':0,'M':1}) # One-hot encode labels
-X = data.drop(columns=['diagnosis']) # Remove target label from features
+data = pd.read_csv("lung-cancer.csv")
+y = data['LUNG_CANCER'].replace({'YES':0, 'NO':1}) # One-hot encode labels
+X = data.drop(columns=['LUNG_CANCER']) # Remove target label from features
 
-# Feature selection
-X = X.drop(columns=['id']) # The id column is not useful information
+# Feature selection/transformation
+X = X.replace({2:0, 'F':0, 'M':1}) # One-hot encode features. Let yes=0 and no=1
 
 # Split into training (70%), validation (20%), and testing (10%)
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, random_state=RANDOM_STATE)
