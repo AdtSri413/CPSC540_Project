d5f3c8add9c12b48e4bf98fb8150fbbe1c9afa01
AdtSri413 <aditi.srinivasan@gmail.com>
2023-12-18 17:50:30 -0500

    Restructured files

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
+from plots import ROC_curve, confusion_mat
+
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 from sklearn.model_selection import GridSearchCV
@@ -7,7 +8,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import RocCurveDisplay, ConfusionMatrixDisplay, confusion_matrix
 
 
 def random_forest(X_train, y_train, X_test, y_test):
@@ -93,28 +93,3 @@ def logistic_regression(X_train, y_train, X_test, y_test):
     ROC_curve(model, X_test, y_test, "Logistic Regression")
     confusion_mat(model, y_hat, y_test, "Logistic Regression")
 
-
-def ROC_curve(model, X_test, y_test, model_name):
-    RocCurveDisplay.from_predictions(
-        y_test.to_numpy(),
-        model.predict_proba(X_test)[:, 1],
-        name=f"ROC Curve - {model_name}",
-        color="darkorange",
-    )
-    plt.plot([0,1],[0,1], "k--")
-    plt.axis("square")
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.title(f"ROC Curve - {model_name}")
-    plt.legend(loc = 'lower right')
-    plt.show()
-
-def confusion_mat(model, y_hat, y_test, model_name):
-    cm = confusion_matrix(y_test, y_hat, labels=model.classes_)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm,
-                                  display_labels=model.classes_)
-    disp.plot()
-    plt.xlabel("Predicted")
-    plt.ylabel("Actual")
-    plt.title(f"Confusion Matrix - {model_name}")
-    plt.show()
